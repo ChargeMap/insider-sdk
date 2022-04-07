144c2c3332b2423e7c072a2dca11d7bdb03e03f7
Gaydamakha <mikhail@chargemap.com>
2022-04-07 15:42:34 +0200

    :bug: add previous exception to insider api exception

--- src/InsiderApiException.php
+++ src/InsiderApiException.php
@@ -5,15 +5,16 @@ declare(strict_types=1);
 namespace Chargemap\InsiderSdk;
 
 use Exception;
+use Throwable;
 
 class InsiderApiException extends Exception
 {
     private InsiderApiErrorCode $internalErrorCode;
 
-    public function __construct(InsiderApiErrorCode $internalErrorCode, string $message, int $code = 0)
+    public function __construct(InsiderApiErrorCode $internalErrorCode, string $message, int $code = 0, Throwable $previous = null)
     {
         $this->internalErrorCode = $internalErrorCode;
-        parent::__construct($message, $code);
+        parent::__construct($message, $code, $previous);
     }
 
     public function getInternalErrorCode(): InsiderApiErrorCode
